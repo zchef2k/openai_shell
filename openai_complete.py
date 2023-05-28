a047baf1a005a375602c7ed65487f1b0c9001f01
Zac Durham <zacdurham@gmail.com>
2023-05-27 22:14:12 -0400

    Update openai_complete.py

--- openai_complete.py
+++ openai_complete.py
@@ -5,12 +5,12 @@ import os, sys
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 prompt=' '.join((sys.argv[1:]))
-#print(prompt.strip())
+
 
 # create a chat completion
 completion = openai.Completion.create(
     model="text-davinci-003", 
-    prompt="Gemerate a linux bash command to " + prompt.strip() + ""
+    prompt="Generate a Linux bash command to " + prompt.strip() + ""
     )
 
 # print the chat completion
