47c753368755ac0a78dddc6cc420af40b284ac7b
Zac Durham <zacdurham@gmail.com>
2023-08-22 16:44:53 -0400

    Switched models and to chat completion

--- openai_complete.py
+++ openai_complete.py
@@ -8,11 +8,10 @@ prompt=' '.join((sys.argv[1:]))
 
 
 # create a chat completion
-completion = openai.Completion.create(
-    model="text-davinci-003", 
-    prompt="Generate a Linux bash command to " + prompt.strip() + ""
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Generate a one line Linux bash command to " + prompt.strip() + "."}]
     )
 
 # print the chat completion
-print(completion.choices[0].text.strip())
-
+print(completion.choices[0].message.content)
